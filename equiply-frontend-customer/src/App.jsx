02764d7eca9a271c6f28235a7c71229ef7ece56f
nitish-002 <surya0424sharma@gmail.com>
2025-05-13 14:38:24 +0530

    first

---
+++ equiply-frontend-customer/src/App.jsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter } from "react-router";
+import { Routes, Route } from "react-router-dom";
+import Homepage from './homepage';
+import Product from './product';
+
+function App() { 
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/product" element={<Product/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ equiply-frontend-customer/src/product/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div><p>Product</p></div>
+  )
+}
+
+export default Product
