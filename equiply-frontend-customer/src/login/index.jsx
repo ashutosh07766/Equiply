d5ffbaf914e154262af9b13a85197e92771976fb
Vaibhav <vaibhavkumawat7605@gmail.com>
2025-05-13 23:15:58 +0530

    Fix login Page Input Size and added Place Holder

--- equiply-frontend-customer/src/login/index.jsx
+++ equiply-frontend-customer/src/login/index.jsx
@@ -4,40 +4,46 @@ import { FcGoogle } from "react-icons/fc"
 
 const Login = () => {
   return (
-    <div className="pt-[72px] pr-[430px] pb-[116px] pl-[430px] flex flex-col gap-[32px]">
+    <div className="min-h-screen bg-gray-50 pt-16 px-4" style={{ fontFamily: "'Poppins', sans-serif" }}>
         <div>
-            <h3 className="text-center justify-center text-zinc-800 text-3xl font-medium font-['Poppins']">Log in</h3>
+            <h3 className="text-center text-4xl mb-1">Log in</h3>
+            <p className="text-center text-lg mb-8">Don't have an account? <Link to="/Signup" className="text-blue-600 hover:underline">Signup</Link></p>
         </div>
-        <div>   
-            <form className="flex flex-col gap-[24px]">
-              <lable className="block mb-1 text-sm text-gray-600">
-                Email address
-                <div>
-                  
-                <input type="text" className="w-full px-4 py-4 border border-gray-300 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"/>
-                </div>
-              </lable>
-              <lable className="block mb-1 text-sm text-gray-600">
-                Password
-                <div>
-                <input type="password" className="w-full px-4 py-4 border border-gray-300 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"/>
-                </div>
-                <label className='flex'>
-                <input type="checkbox"/>
-                <p className='p-2'>Remember me</p>
-              </label>
-              </lable>
+        <div className="flex items-center justify-center px-4">   
+            <form className="w-full max-w-2xl p-6 space-y-6">
               <div>
-              <p className="text-xs text-gray-600">By creating an account, you agree to the{" "}<a href="#" className="text-blue-600 underline">Terms of use</a>{" "}and{" "}<a href="#" className="text-blue-600 underline">Privacy Policy</a>.</p>
-              <button className="mt-[2px] w-full py-3 bg-gray-200 text-gray-500 font-medium text-sm rounded-full cursor-not-allowed pt-[2]" >Log in</button>
+                <label className="block mb-1 text-sm text-gray-600">
+                  Email address
+                  <input type="text" placeholder="Enter your email address" required className="w-full px-4 py-4 border border-gray-300 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"/>
+                </label>
               </div>
-            </form> 
-            <div className='mt-[24px] flex flex-col gap-[24px] text-center'>
-            <p className="justify-start text-neutral-900 text-base font-medium font-['Poppins'] underline">Forgot Password?</p>
-              <p className="text-stone-500 text-base font-normal font-['Poppins']">Don't have an account? <Link to="/Signup" className="  text-neutral-900 text-base font-medium font-['Poppins'] underline">Signup</Link></p>
+              
+              <div>
+                <label className="block mb-1 text-sm text-gray-600">
+                  Password
+                  <input type="password" placeholder="Enter your password" required className="w-full px-4 py-4 border border-gray-300 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"/>
+                </label>
+                <label className='flex mt-2'>
+                  <input type="checkbox"/>
+                  <p className='p-2'>Remember me</p>
+                </label>
               </div>
-              <p className='text-center mt-[48px] text-sm text-gray-500'>Or continue with</p>
-             <button type="button"className="flex items-center justify-center w-1/4 mx-auto border border-gray-300 py-3 rounded-full hover:bg-gray-100 transition"><FcGoogle className="text-xl mr-2" /> Google</button>
+              
+              <div>
+                <p className="text-xs text-gray-600">By creating an account, you agree to the{" "}<a href="#" className="text-blue-600 underline">Terms of use</a>{" "}and{" "}<a href="#" className="text-blue-600 underline">Privacy Policy</a>.</p>
+                <button className="mt-4 w-full py-3 bg-gray-200 text-gray-500 font-medium text-sm rounded-full cursor-not-allowed">Log in</button>
+              </div>
+              
+              <div className='text-center'>
+                <p className="text-neutral-900 text-base font-medium underline">Forgot Password?</p>
+              </div>
+              
+              <div className="text-center text-sm text-gray-500">OR Continue with</div>
+              
+              <button type="button" className="flex items-center justify-center w-1/4 mx-auto border border-gray-300 py-3 rounded-full hover:bg-gray-100 transition">
+                <FcGoogle className="text-xl mr-2" /> Google
+              </button>
+            </form>
         </div>
     </div>
   )
