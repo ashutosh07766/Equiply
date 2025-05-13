0554e6841329908242551de19e02caceaeb8f855
Vaibhav <vaibhavkumawat7605@gmail.com>
2025-05-13 15:13:40 +0530

    Added Routes to all pages

--- equiply-frontend-customer/src/App.jsx
+++ equiply-frontend-customer/src/App.jsx
@@ -1,6 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './homepage';
 import Product from './product';
+import Cheakout from './Cheakout';
+import ProductVeiw from './Productveiw';
+import Signup from './Signup';
+import History from "./History";
+import Payment from "./Payment";
+import Profile from "./Profile";
+import PutRent from "./PutRent";
+import Wishlist from "./Wishlist";
+import OrderVeiw from "./OrderVeiw";
 
 function App() { 
 
@@ -9,6 +18,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/product" element={<Product/>}/>
+        <Route path="/Cheakout" element={<Cheakout/>}/>
+        <Route path="/ProductVeiw" element={<ProductVeiw/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
+        <Route path="/History" element={<History/>}/>
+        <Route path="/Payment" element={<Payment/>}/>
+        <Route path="/Profile" element={<Profile/>}/>
+        <Route path="/PutRent" element={<PutRent/>}/>
+        <Route path="/Wishlist" element={<Wishlist/>}/>
+        <Route path="/OrderVeiw" element={<OrderVeiw/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ equiply-frontend-customer/src/PutRent/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PutRent = () => {
+  return (
+    <div><p>PutRent</p></div>
+  )
+}
+
+export default PutRent
